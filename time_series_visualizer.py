ab53778e423685b9692a196f6fe9e5c93c243688
JosueRosado <168879068+JosueRosado@users.noreply.github.com>
2024-05-03 20:18:31 -0500

    Update time_series_visualizer.py

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,48 +5,37 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv("fcc-forum-pageviews.csv")
-df["date"] = pd.to_datetime(df["date"])
+df = pd.read_csv('fcc-forum-pageviews.csv')
+df['date'] = pd.to_datetime(df['date'])
 df = df.set_index('date')
 
 # Clean data
-df = df.loc[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
-
+df = df.drop(df[(df.value<df.value.quantile(0.025)) | (df.value>df.value.quantile(0.975))].index)
 
 def draw_line_plot():
     # Draw line plot
-    fig, ax = plt.subplots(figsize=(32, 10), dpi=100)
-    ax.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=100)
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
     ax.set_xlabel("Date")
     ax.set_ylabel("Page Views")
-    sns.lineplot(data=df, legend=False)
+    plt.plot(df);
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
 
 def draw_bar_plot():
-    # Copy and modify data for monthly bar plot
-    df_bar = df.copy()
-    df_bar["Years"] = df_bar.index.year
-    df_bar["Months"] = df_bar.index.month_name()
-    df_bar = pd.DataFrame(df_bar.groupby(["Years", "Months"], sort=False)["value"].mean().round().astype(int))
-    df_bar = df_bar.rename(columns={"value": "Average Page Views"})
-    df_bar = df_bar.reset_index()
-    missing_data = {
-        "Years": [2016, 2016, 2016, 2016],
-        "Months": ['January', 'February', 'March', 'April'],
-        "Average Page Views": [0, 0, 0, 0]
-    }
-
-    df_bar = pd.concat([pd.DataFrame(missing_data), df_bar])
+   df_bap = df.copy()
+   df_bap["Years"] = df_bap.index.year
+   df_bap["Months"] = df_bap.index.month_name()
+   df_bap = pd.DataFrame(df_bap.groupby(["Years", "Months"], sort=False)["value"].mean().astype(int))
+   df_bap = df_bap.reset_index()
 
     # Draw bar plot
-    fig, ax = plt.subplots(figsize=(19.2, 10.8), dpi=100)
-    ax.set_title("Daily freeCodeCamp Forum Average Page Views per Month")
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=100)
+    ax.set_title("Months")
 
-    chart = sns.barplot(data=df_bar, x="Years", y="Average Page Views", hue="Months", palette="tab10")
-    chart.set_xticklabels(chart.get_xticklabels(), rotation=90, horizontalalignment='center')
+    sns.barplot(data=df_bap, x="Years", y="value", hue='Months');
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
@@ -60,20 +49,19 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
-    fig, axes = plt.subplots(1, 2, figsize=(32, 10), dpi=100)
+    fig, ax = plt.subplots(1, 2, figsize=(24, 6), dpi=100)
     
     # Yearly boxplot
-    sns.boxplot(data=df_box, x="year", y="value", ax=axes[0])
-    axes[0].set_title("Year-wise Box Plot (Trend)")
-    axes[0].set_xlabel("Year")
-    axes[0].set_ylabel("Page Views")
+    sns.boxplot(data=df_box, x="year", y="value", ax=ax[0])
+    ax[0].set_title("Year-wise Box Plot (Trend)")
+    ax[0].set_xlabel("Year")
+    ax[0].set_ylabel("Page Views")
     
     # Monthly boxplot
-    month_order = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    sns.boxplot(data=df_box, x="month", y="value", order=month_order, ax=axes[1])
-    axes[1].set_title("Month-wise Box Plot (Seasonality)")
-    axes[1].set_xlabel("Month")
-    axes[1].set_ylabel("Page Views")
+    sns.boxplot(data=df_bop, x="month", y="value", ax=ax[1])
+    ax[1].set_title("Month-wise Box Plot (Seasonality)")
+    ax[1].set_xlabel("Month")
+    ax[1].set_ylabel("Page Views");
 
     # Save image and return fig (don't change this part)
     fig.savefig('box_plot.png')
